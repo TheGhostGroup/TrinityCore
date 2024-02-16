ba3058d2c48481802c57f923b25131d460de0a77
TheGhostGroup <theghostgroup@gmail.com>
2024-02-16 22:14:18 +1300

    Core/Script: Initial eye of azshara data.

--- src/server/scripts/BrokenIsles/EyeOfAzshara/instance_eye_of_azshara.cpp
+++ src/server/scripts/BrokenIsles/EyeOfAzshara/instance_eye_of_azshara.cpp
@@ -15,6 +15,8 @@
 * with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "instance_eye_of_azshara.h"
+
 class instance_eye_of_azshara : public InstanceMapScript
 {
 public:
