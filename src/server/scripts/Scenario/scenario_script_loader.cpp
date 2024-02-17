03679b5026552bc68ed3dce4649cdf188690a301
TheGhostGroup <theghostgroup@gmail.com>
2024-02-17 13:58:51 +1300

    Core/Script: Initial battle for broken shore scenario data.

--- src/server/scripts/Scenario/scenario_script_loader.cpp
+++ src/server/scripts/Scenario/scenario_script_loader.cpp
@@ -16,11 +16,11 @@
 */
 
 // This is where scripts' loading functions should be declared:
-
+void AddSC_instance_broken_shore_scenario();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddScenarioScripts()
 {
-
+    AddSC_instance_broken_shore_scenario();
 }
