23cf25196fcf2e4f9a4b3c571bd825192636db88
TheGhostGroup <theghostgroup@gmail.com>
2024-02-21 19:32:56 +1300

    DB/Creature: add missing creature text for Sergeant Dalton.

---
+++ sql/updates/world/master/2024_02_21_00_world.sql
@@ -0,0 +1,6 @@
+DELETE FROM `creature_text` WHERE `CreatureID`=108961;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextID`, `TextRange`, `comment`) VALUES 
+(108961, 0, 0, 'Commander! I have a message for you!', 12, 0, 100, 5, 0, 0, 114677, 0, 'Sergeant Dalton to Player'),
+(108961, 3, 0, 'Excellent, commander. Danath awaits you on Krasus\' Landing.', 12, 0, 100, 66, 0, 0, 114680, 0, 'Sergeant Dalton to Player'),
+(108961, 1, 0, 'I bear an important message from Danath Trollbane!', 12, 0, 100, 5, 0, 0, 114678, 0, 'Sergeant Dalton to Player'),
+(108961, 2, 0, 'If you please, this is an urgent matter that requires your attention!', 12, 0, 100, 6, 0, 0, 114679, 0, 'Sergeant Dalton to Player');
