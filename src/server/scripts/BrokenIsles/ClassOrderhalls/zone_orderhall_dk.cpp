52544e7a10048349418f6d326f538b9dcc96833e
TheGhostGroup <theghostgroup@gmail.com>
2024-04-11 20:15:15 +1200

    Core/Script: Initial death knight orderhall intro data.
    * Add initial enum data for death knight orderhall intro.

--- src/server/scripts/BrokenIsles/ClassOrderhalls/zone_orderhall_dk.cpp
+++ src/server/scripts/BrokenIsles/ClassOrderhalls/zone_orderhall_dk.cpp
@@ -17,6 +17,27 @@
 
 #include "ScriptMgr.h"
 
+enum DukeLankral
+{
+    // DK Intro Quests
+    QUEST_THE_CALL_TO_WAR              = 40714,
+    QUEST_A_PACT_OF_NECESSITY          = 40715,
+
+    SPELL_PLAYER_CHOICE                = 199985,
+
+    PLAYER_CHOICE_DK_BLOOD             = 541,
+    PLAYER_CHOICE_DK_FROST             = 542,
+    PLAYER_CHOICE_DK_UNHOLY            = 543,
+
+    KILL_CREDIT_ARTIFACT_WEAPON_CHOSEN = 101441,
+    // Blood Artifact Quest
+    QUEST_THE_DEAD_AND_THE_DAMNED      = 40740,
+    // Frost Artifact Quest
+    QUEST_THE_CALL_OF_ICECROWN         = 38990,
+    // Unholy Artifact Quest
+    QUEST_APOCALYPSE                   = 40930,
+};
+
 void AddSC_zone_orderhall_dk()
 {
 }
