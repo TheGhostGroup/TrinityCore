d72d392735ef07c8fd89d632e1423416e83f6be2
TheGhostGroup <theghostgroup@gmail.com>
2024-05-05 13:12:01 +1200

    Core/Misc: rename and update copyright.

--- src/server/game/Globals/BattlePayDataStore.h
+++ src/server/game/Globals/BattlePayDataStore.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2008-2023 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2023 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2008-2024 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2024 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
