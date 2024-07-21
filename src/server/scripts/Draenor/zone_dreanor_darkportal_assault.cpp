e0531f130a2b23cbef5de222e3bf2e212ca881bf
TheGhostGroup <theghostgroup@gmail.com>
2024-07-21 20:39:35 +1200

    Core/Script: Add initial dark portal assault script holder
    * Add script holder
    * Define initial entries

---
+++ src/server/scripts/Draenor/zone_dreanor_darkportal_assault.cpp
@@ -0,0 +1,51 @@
+/*
+* This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+enum Creatures
+{
+    NPC_ARCHMAGE_KHADGAR_1              = 78558,
+    NPC_THAELIN_DARKANVIL               = 78568,
+    NPC_HANSEL_HEAVYHANDS               = 78569,
+    NPC_TERONGOR                        = 81696,
+    NPC_CHOGALL                         = 81695,
+    NPC_KHADGAR_SHIELD_TARGET_DUMMY     = 82305,
+    NPC_GULDAN                          = 78333,
+};
+
+enum Quests
+{
+    // Dark Portal
+    QUEST_THE_DARK_PORTAL               = 34398,
+    QUEST_ONSLAUGHTS_END                = 34392,
+    QUEST_THE_PORTALS_POWER             = 34393,
+    QUEST_AZEROTHS_LAST_STAND           = 35933,
+};
+
+enum Scenes
+{
+    SCENE_CHOGALL_FREED                 = 961,
+    SCENE_TERONGOR_FREED                = 962,
+    SCENE_DETONATION_DWARVES            = 937,
+    SCENE_GULDAN_REVEAL                 = 925,
+    SCENE_GULDAN_FREED                  = 808,
+};
+
+
+void AddSC_zone_darkportal_assault()
+{
+
+}
