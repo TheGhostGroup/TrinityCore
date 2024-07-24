b9643b5bf3fbf5c3f6e98438ec971f4a3bb6b1d7
TheGhostGroup <theghostgroup@gmail.com>
2024-07-24 22:19:02 +1200

    DB/Creature: dalaran broken isles clean up.
    * Relocate spawns to correct range for expansion.

---
+++ sql/updates/world/master/2024_07_24_01_world.sql
@@ -0,0 +1,35 @@
+-- dalaran cleanup
+
+SET @CGUID := 600030;
+
+UPDATE `creature` SET `guid`=@CGUID+0 WHERE `guid`=20508271;
+UPDATE `creature` SET `guid`=@CGUID+1 WHERE `guid`=20508253;
+UPDATE `creature` SET `guid`=@CGUID+2 WHERE `guid`=20508251;
+UPDATE `creature` SET `guid`=@CGUID+3 WHERE `guid`=20508342;
+UPDATE `creature` SET `guid`=@CGUID+4 WHERE `guid`=20508345;
+UPDATE `creature` SET `guid`=@CGUID+5 WHERE `guid`=20508408;
+UPDATE `creature` SET `guid`=@CGUID+6 WHERE `guid`=20508409;
+UPDATE `creature` SET `guid`=@CGUID+7 WHERE `guid`=20508426;
+UPDATE `creature` SET `guid`=@CGUID+8 WHERE `guid`=20508432;
+UPDATE `creature` SET `guid`=@CGUID+9 WHERE `guid`=20508429;
+UPDATE `creature` SET `guid`=@CGUID+10 WHERE `guid`=20508448;
+UPDATE `creature` SET `guid`=@CGUID+11 WHERE `guid`=20508450;
+UPDATE `creature` SET `guid`=@CGUID+12 WHERE `guid`=20508467;
+UPDATE `creature` SET `guid`=@CGUID+13 WHERE `guid`=20508466;
+UPDATE `creature` SET `guid`=@CGUID+14 WHERE `guid`=20508478;
+UPDATE `creature` SET `guid`=@CGUID+15 WHERE `guid`=20508479;
+UPDATE `creature` SET `guid`=@CGUID+16 WHERE `guid`=20508480;
+UPDATE `creature` SET `guid`=@CGUID+17 WHERE `guid`=20508482;
+UPDATE `creature` SET `guid`=@CGUID+18 WHERE `guid`=20508487;
+UPDATE `creature` SET `guid`=@CGUID+19 WHERE `guid`=20508515;
+UPDATE `creature` SET `guid`=@CGUID+20 WHERE `guid`=20508484;
+UPDATE `creature` SET `guid`=@CGUID+21 WHERE `guid`=20508485;
+UPDATE `creature` SET `guid`=@CGUID+22 WHERE `guid`=20508524;
+UPDATE `creature` SET `guid`=@CGUID+23 WHERE `guid`=20508530;
+UPDATE `creature` SET `guid`=@CGUID+24 WHERE `guid`=20508540;
+UPDATE `creature` SET `guid`=@CGUID+25 WHERE `guid`=20508546;
+UPDATE `creature` SET `guid`=@CGUID+26 WHERE `guid`=20508550;
+UPDATE `creature` SET `guid`=@CGUID+27 WHERE `guid`=20508549;
+UPDATE `creature` SET `guid`=@CGUID+28 WHERE `guid`=20508961;
+UPDATE `creature` SET `guid`=@CGUID+29 WHERE `guid`=20508473;
+UPDATE `creature` SET `guid`=@CGUID+30 WHERE `guid`=20508474;
