e3ad1b85990cc07e290100eae32c625c9587369f
TheGhostGroup <theghostgroup@gmail.com>
2024-07-27 18:53:24 +1200

    DB/Creature: Assign script to creatures.
    * Hansel Heavyhands
    * Thaelin Darkanvil.

---
+++ sql/updates/world/master/2024_07_27_01_world.sql
@@ -0,0 +1,4 @@
+-- assign script to hansel & thaelin
+
+UPDATE `creature_template` SET `ScriptName`='npc_hansel_heavyhands_78569' WHERE `entry`=78569;
+UPDATE `creature_template` SET `ScriptName`='npc_thaelin_darkanvil_78568' WHERE `entry`=78568;
