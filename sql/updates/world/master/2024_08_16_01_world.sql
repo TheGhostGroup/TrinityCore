63e6fe04fb573a5f70b746ad5718005933dffa0c
TheGhostGroup <theghostgroup@gmail.com>
2024-08-16 20:01:14 +1200

    DB/Creature: mage orderhall clean up part 2
    * Relocate spawns to correct range for expansion.

---
+++ sql/updates/world/master/2024_08_16_01_world.sql
@@ -0,0 +1,28 @@
+-- mage orderhall cleanup
+
+SET @CGUID := 600147;
+
+UPDATE `creature` SET `guid`=@CGUID+0 WHERE `guid`=20540890;
+UPDATE `creature` SET `guid`=@CGUID+1 WHERE `guid`=20540900;
+UPDATE `creature` SET `guid`=@CGUID+2 WHERE `guid`=20540896;
+UPDATE `creature` SET `guid`=@CGUID+3 WHERE `guid`=20540899;
+UPDATE `creature` SET `guid`=@CGUID+4 WHERE `guid`=20540902;
+UPDATE `creature` SET `guid`=@CGUID+5 WHERE `guid`=20540897;
+UPDATE `creature` SET `guid`=@CGUID+6 WHERE `guid`=20540898;
+UPDATE `creature` SET `guid`=@CGUID+7 WHERE `guid`=20540901;
+UPDATE `creature` SET `guid`=@CGUID+8 WHERE `guid`=20540909;
+UPDATE `creature` SET `guid`=@CGUID+9 WHERE `guid`=20540907;
+UPDATE `creature` SET `guid`=@CGUID+10 WHERE `guid`=20540906;
+UPDATE `creature` SET `guid`=@CGUID+11 WHERE `guid`=20540883;
+UPDATE `creature` SET `guid`=@CGUID+12 WHERE `guid`=20540855;
+UPDATE `creature` SET `guid`=@CGUID+13 WHERE `guid`=20540871;
+UPDATE `creature` SET `guid`=@CGUID+14 WHERE `guid`=20540864;
+UPDATE `creature` SET `guid`=@CGUID+15 WHERE `guid`=20540884;
+UPDATE `creature` SET `guid`=@CGUID+16 WHERE `guid`=20540886;
+UPDATE `creature` SET `guid`=@CGUID+17 WHERE `guid`=20540905;
+UPDATE `creature` SET `guid`=@CGUID+18 WHERE `guid`=20540908;
+UPDATE `creature` SET `guid`=@CGUID+19 WHERE `guid`=20540910;
+UPDATE `creature` SET `guid`=@CGUID+20 WHERE `guid`=20540885;
+UPDATE `creature` SET `guid`=@CGUID+21 WHERE `guid`=20540879;
+
+
