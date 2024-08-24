7e81eef0ede9ef3e28ed5a76effd1faae5a135ff
TheGhostGroup <theghostgroup@gmail.com>
2024-08-24 21:27:52 +1200

    DB/Scenario: Vault of Wardens

---
+++ sql/updates/world/master/2024_08_24_04_world.sql
@@ -0,0 +1,7 @@
+-- Scenaro Vault of Wardens
+DELETE FROM `scenarios` WHERE `map`=1493;
+INSERT INTO `scenarios` (map, difficulty, scenario_A, scenario_H) VALUES
+(1493, 23, 1258, 1258),
+(1493, 8, 1173, 1173),
+(1493, 2, 1005, 1005),
+(1493, 1, 1005, 1005);
