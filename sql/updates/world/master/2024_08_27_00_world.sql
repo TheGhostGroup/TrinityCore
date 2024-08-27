b6d8ed92172e339d05792204f280606cd87433a6
TheGhostGroup <theghostgroup@gmail.com>
2024-08-27 21:36:42 +1200

    DB/Creature: Add missing creature text entries for khadgar raven form.

---
+++ sql/updates/world/master/2024_08_27_00_world.sql
@@ -0,0 +1,9 @@
+DELETE FROM `creature_text` WHERE `CreatureID`=103660;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(103660, 0, 0, 'I prefer using the greatstaff Atiesh\'s raven form. Nothing\'s worse than saddle sores.', 12, 0, 100, 0, 0, 58376, 0, 0, 'Archmage Khadgar to Player'),
+(103660, 1, 1, 'The demon hunters have set up camp on the far side of the island. Perhaps they can help us locate the Pillar of Creation.', 12, 0, 100, 0, 0, 58375, 0, 0, 'Archmage Khadgar to Player'),
+(103660, 2, 2, 'Look there... naga forces. We have competition from Queen Azshara herself!', 12, 0, 100, 0, 0, 58374, 0, 0, 'Archmage Khadgar to Player'),
+(103660, 3, 3, 'She must also be after the Pillar of Creation. This is unexpected.', 12, 0, 100, 0, 0, 58373, 0, 0, 'Archmage Khadgar to Player'),
+(103660, 4, 4, 'Breathtaking. Imagine all of the arcane knowledge lost to the ages here.', 12, 0, 100, 0, 0, 60191, 0, 0, 'Archmage Khadgar to Player'),
+(103660, 5, 5, 'There... the Illidari. And, the Burning Legion is here, too.', 12, 0, 100, 0, 0, 60192, 0, 0, 'Archmage Khadgar to Player');
+
