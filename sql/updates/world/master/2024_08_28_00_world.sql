98624e35a7a84be26df1801bbfe5ddbd28fa1921
TheGhostGroup <theghostgroup@gmail.com>
2024-08-28 21:10:03 +1200

    DB/Scenario: Halls of Valor.

---
+++ sql/updates/world/master/2024_08_28_00_world.sql
@@ -0,0 +1,7 @@
+-- Halls of Valor
+DELETE FROM `scenarios` WHERE `map`=1477;
+INSERT INTO `scenarios` (map, difficulty, scenario_A, scenario_H) VALUES
+(1477, 23, 1255, 1255),
+(1477, 8, 1046, 1046),
+(1477, 2, 932, 932),
+(1477, 1, 929, 929);
