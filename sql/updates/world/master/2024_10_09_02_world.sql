d9e78186332750145d1c1a1d9a17243423104fd2
TheGhostGroup <theghostgroup@gmail.com>
2024-10-09 19:48:19 +1300

    DB/Condition: Blink of an eye horde in phase 5829

---
+++ sql/updates/world/master/2024_10_09_02_world.sql
@@ -0,0 +1,4 @@
+-- Conditions
+
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `Comment`) VALUES
+(26, 5829, 41, 0, 0, 47, 0, 44184, 2 | 8, 0, 0, 'Allow phase 5829 if quest Inprogress | Completed NOT Rewarded'); -- Blink of an eye Horde
